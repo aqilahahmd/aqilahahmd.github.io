c99d47e2cd94b148d2c764c0a42033a465d9cdb8
Aqilah Ahmad <aqilahmad1997@gmail.com>
2021-04-07 20:34:56 +0800

    2034

--- vuejs/js/index.js
+++ vuejs/js/index.js
@@ -4,3 +4,11 @@ let app = new VTTCue({
        message:"Hello Vue!"
    } 
 });
+
+let app2 = new VTTCue({
+    el: "#app2",
+    data: {
+        message: "Loaded on " + new Date().toLocaleString()
+    }
+
+};)
