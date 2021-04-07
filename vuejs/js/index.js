0c41d9dfb879976074fa0071124286dd658488fc
Aqilah Ahmad <aqilahmad1997@gmail.com>
2021-04-07 20:43:54 +0800

    2143

--- vuejs/js/index.js
+++ vuejs/js/index.js
@@ -8,7 +8,16 @@ let app = new VTTCue({
 let app2 = new VTTCue({
     el: "#app2",
     data: {
-        message: "Loaded on " + new Date().toLocaleString()
+        message: "Loaded on " + new Date().toLocaleString(),
+        linkUrl: "https://bbc.in/2LAnJs8",
+        linkText: "Google is down"
     }
 
 };)
+
+let app3 = new Vue ({
+    el: "app3",
+    data: {
+        showIt:true
+    }
+});
