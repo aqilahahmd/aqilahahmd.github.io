5fd196507e31f4a8540c3a55055310037c315d45
Aqilah Ahmad <aqilahmad1997@gmail.com>
2021-04-09 21:42:30 +0800

    2142

--- tableapp/js/list.js
+++ tableapp/js/list.js
@@ -3,3 +3,28 @@ refreshNowBtn.addEventListener("click",function(){
     alert("GetBooking");
 });
 
+function GetBooking(){
+    let url = 'https://api.sheety.co/14e5dbeb43f50147a8373c58a5a3b6e5/tableapp/bookingList';
+    fetch(url)
+    .then((response) => response.json())
+    .then(json => {
+
+        let bookingNameList = document.getElementById("bookingNameList");
+
+        for(let i = 0; i < json.bookingitem.length; i++){
+            let gName = json.bookingitems[i].name;
+            let gEmail = json.bookingitems[i].email;
+            let gPax = json.bookingitems[i].pax;
+            let gRemarks = json.bookingitems[i].remarks;
+            let gId = json.bookingitems[i].id;
+
+            let row = bookingNameList.insertRow(bookingNameList.rows.length);
+            row.insertCell(0).innerHTML = gId;
+            row.insertCell(1).innerHTML = gName;
+            row.insertCell(2).innerHTML = gEmail;
+            row.insertCell(3).innerHTML = gPax;
+            row.insertCell(4).innerHTML = gRemarks;
+            row.insertCell(5).innerHTML = "TBC";
+        }
+});
+}
