c0a3a76c4cb85e8483086af728c8b68471b08452
Aqilah Ahmad <aqilahmad1997@gmail.com>
2021-04-12 19:23:54 +0800

    1923

---
+++ tableapp/js/book.js
@@ -0,0 +1,17 @@
+let bookNowBtn = documentt.getElementById("bookNow");
+bookNowBtn.addEventListener("click",function(){
+    let userName = document.getElementById("userName");
+    let userNameVal = userName.value;
+
+    let userEmail = document.getElementById("userEmail");
+    let UserEmailVal = userEmail.value;
+
+    let userPaxVal = document.getElementById("userPax").value;
+    let userRemarksVal = document.getElementById("userRemarks0").value;
+
+    bookNowBtn(userNameVal, UserEmailVal, userPaxVal, userRemarksVal);
+});
+
+function BookNow(userName,userEmail,userPax,userRemarks){
+    alert(userName);
+}
